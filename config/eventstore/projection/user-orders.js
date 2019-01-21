df84fc6c048bb83ee5c6d08227b533a87d10361b
gabrielfs7 <gabrielfs7@gmail.com>
2019-01-21 21:12:36 +0100

    Projection for order event

--- config/eventstore/projection/user-orders.js
+++ config/eventstore/projection/user-orders.js
@@ -23,6 +23,21 @@ fromCategory(':fromCategory')
                     productSku: event.body.productSku
                 }
             );
+        },
+        OrderUpdated: function(state, event) {
+            for (var i = 0; i <= state.data.length; i++) {
+                if (state.data[i].orderId == event.body.id) {
+                    state.data[i] = {
+                        orderId: event.body.id,
+                        orderCreatedAt: event.body.createdAt,
+                        orderAmount: event.body.amount,
+                        userId: event.body.userId,
+                        userFullName: event.body.userFullName,
+                        productName: event.body.productName,
+                        productSku: event.body.productSku
+                    }
+                }
+            }
         }
     }
 )
