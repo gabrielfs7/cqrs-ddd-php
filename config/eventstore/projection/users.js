885caae87ad79291c39ffdcdb74e3e8c4fbac382
gabrielfs7 <gabrielfs7@gmail.com>
2019-01-21 21:23:27 +0100

    Projections update total orders and user

--- config/eventstore/projection/users.js
+++ config/eventstore/projection/users.js
@@ -18,9 +18,31 @@ fromCategory(':fromCategory')
                         userId: event.body.id,
                         fullName: event.body.fullName,
                         username: event.body.username,
-                        birthday: event.body.birthday
+                        birthday: event.body.birthday,
+                        totalOrders: 0,
+                        totalOrderAmount: 0
                     }
                 );
+            }
+        },
+        UserUpdated: function(state, event) {
+            for (var i = 0; i <= state.data.length; i++) {
+                if (state.data[i].userId == event.body.id) {
+                    state.data[i] = {
+                        userId: event.body.id,
+                        fullName: event.body.fullName,
+                        username: event.body.username,
+                        birthday: event.body.birthday
+                    }
+                }
+            }
+        },
+        OrderCreated: function(state, event) {
+            for (var i = 0; i <= state.data.length; i++) {
+                if (state.data[i].userId == event.body.userId) {
+                    state.data[i].totalOrders++;
+                    state.data[i].totalOrderAmount += event.body.amount;
+                }
             }
         }
     }
